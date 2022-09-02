c59763f8facc12f62a4a994f4aaefb856b38040c
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-09-03 02:31:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//global middlewares
+app.use(cors());
+app.use(express.json());
+app.set("view engine", "ejs");
+
+//routes
+
+
+
+
+
+
+app.get("/", (req, res) => {
+   res.render("home.ejs",{
+    port: port
+   })
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
+
+//for no routes found
+app.all("*",(req, res)=>{
+    res.send("No route found")
+  });
+
+
+//for error which express can't handle'
+
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
